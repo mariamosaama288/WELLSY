20c59e251973456cd8d96a2ca4cb96295718c496
412009 <daliamohamed412009@gmail.com>
2025-10-17 10:03:43 +0300

    Update App.jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,13 +2,11 @@ import { BrowserRouter,Route,Routes } from "react-router-dom";
 import Meals from "./meals/meals.jsx";
 import Mood from './mood/mood.jsx';
 import HomePage from "./homepage/home.jsx";
-import Chatbot from "./chat/chatbot.jsx";
 function App() {
   return (
        <>
       <BrowserRouter>
       <Routes>
-       <Route path='/chatbot' exact element={<Chatbot/>}/>
       <Route path='/' exact element={<HomePage/>}/>
      <Route path='/meals' exact element={<Meals/>}/>
       <Route path='/mood' exact element={<Mood/>}/>
